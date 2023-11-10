ed8a8ef0fd7a7c9b7fdd51d2d5c19b33d437bb87
User <User@192.168.1.4>
2023-11-10 16:04:06 +0700

    code ThongKe, bieu do lay du lieu tu DATABASE

--- src/main/java/com/poly/controller/adminControllers/ProductAdminController.java
+++ src/main/java/com/poly/controller/adminControllers/ProductAdminController.java
@@ -1,11 +1,24 @@
 package com.poly.controller.adminControllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.text.SimpleDateFormat;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.poly.dao.DataDAO;
+import com.poly.entity.Data;
 
 @Controller
 public class ProductAdminController {
+	@Autowired
+	DataDAO dataDAO;
+
 
     @RequestMapping("/admin/index")
     public String Dashboard(Model model) {
@@ -31,5 +44,36 @@ public class ProductAdminController {
     public String Order(Model model) {
         return "admin/order";
     }
+    
+    @RequestMapping("/statistical")
+    public String Statis(Model model) {
+        return "admin/statistical";
+    }
+    
+//    @RequestMapping("/ac")
+//    public String ac(Model model) {
+//        return "admin/ac";
+//    }
+
+    @RequestMapping("/thongke")
+	@ResponseBody
+	public String getDataFromDB() {
+		List<Data> dataList = dataDAO.findAll();
+		
+		
+		JsonArray jsonDate = new JsonArray();
+		JsonArray jsonCount = new JsonArray();
+		JsonObject json = new JsonObject();
+		dataList.forEach(data->{
+			SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+	        String strDate = formatter.format(data.getDate());
+			jsonDate.add(strDate);
+			jsonCount.add(data.getCounter());
+			System.out.println(strDate);
+		});
+		json.add("date", jsonDate);
+		json.add("counter", jsonCount);
+		return json.toString();
+	}
 
 }
