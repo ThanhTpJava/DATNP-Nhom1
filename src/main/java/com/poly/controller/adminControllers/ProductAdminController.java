c2c58e05e97c6f74d17029ecd4cc2d39ecfe6521
ThanhTpJava <tp.thanhjava@gmail.com>
2023-11-11 23:58:03 +0700

    Dev_Thanh&&DEV_Cuong fix UI in Admin

--- src/main/java/com/poly/controller/adminControllers/ProductAdminController.java
+++ src/main/java/com/poly/controller/adminControllers/ProductAdminController.java
@@ -1,8 +1,13 @@
 package com.poly.controller.adminControllers;
 
+import java.text.SimpleDateFormat;
 import java.util.Iterator;
 import java.util.List;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.poly.dao.DataDAO;
+import com.poly.entity.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,13 +17,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.poly.dto.AccountDTO;
 import com.poly.entity.Account;
 import com.poly.service.AccountService;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProductAdminController {
 
 	@Autowired
 	AccountService accService;
-	
+
+    @Autowired
+    DataDAO dataDAO;
+
     @RequestMapping("/admin/index")
     public String Dashboard(Model model) {
         return "admin/index";
@@ -52,4 +61,34 @@ public class ProductAdminController {
 //
 //    }
 
+    @RequestMapping("/statistical")
+    public String Statis(Model model) {
+        return "admin/statistical";
+    }
+
+//    @RequestMapping("/ac")
+//    public String ac(Model model) {
+//        return "admin/ac";
+//    }
+
+    @RequestMapping("/thongke")
+    @ResponseBody
+    public String getDataFromDB() {
+        List<Data> dataList = dataDAO.findAll();
+
+
+        JsonArray jsonDate = new JsonArray();
+        JsonArray jsonCount = new JsonArray();
+        JsonObject json = new JsonObject();
+        dataList.forEach(data->{
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+            String strDate = formatter.format(data.getDate());
+            jsonDate.add(strDate);
+            jsonCount.add(data.getCounter());
+            System.out.println(strDate);
+        });
+        json.add("date", jsonDate);
+        json.add("counter", jsonCount);
+        return json.toString();
+    }
 }
