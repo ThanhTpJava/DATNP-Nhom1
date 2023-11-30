cf141d87f0173e66b2838e7ff234b7070dd5bcdc
QuocCuong1911 <ngocuong1911@gmail.com>
2023-11-30 21:08:42 +0700

    design send email form

--- src/main/java/com/poly/service/impl/EmailServiceImpl.java
+++ src/main/java/com/poly/service/impl/EmailServiceImpl.java
@@ -87,13 +87,69 @@ public class EmailServiceImpl implements EmailService {
 
     @Override
     public void sendOTPResetPassword(Account account, String otp, String subject) {
-        // TODO Auto-generated method stub
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("khoisieudeptrai2304@gmail.com");
-        message.setTo(account.getEmail());
-        message.setSubject(subject);
-        message.setText("Mã OTP của bạn là: " + otp);
-        javaMailSender.send(message);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("khoisieudeptrai2304@gmail.com");
+            helper.setTo(account.getEmail());
+            helper.setSubject(subject);
+
+            String content  = "<!DOCTYPE html>"
+                    + "<html lang='en'>"
+                    + "<head>"
+                    + "<meta charset='UTF-8'>"
+                    + "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+                    + "<title>Xác nhận đơn hàng</title>"
+                    + "<style>"
+                    + "body {"
+                    + "    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;"
+                    + "    line-height: 1.6;"
+                    + "    color: #333;"
+                    + "    margin: 0;"
+                    + "    padding: 0;"
+                    + "}"
+                    + ".container {"
+                    + "    width: 80%;"
+                    + "    margin: 0 auto;"
+                    + "}"
+                    + "header {"
+                    + "    background-color: #007bff;"
+                    + "    color: #fff;"
+                    + "    text-align: center;"
+                    + "    padding: 20px 0;"
+                    + "}"
+                    + "section {"
+                    + "    padding: 20px;"
+                    + "}"
+                    + "footer {"
+                    + "    background-color: #f4f4f4;"
+                    + "    padding: 10px 0;"
+                    + "    text-align: center;"
+                    + "}"
+                    + "</style>"
+                    + "</head>"
+                    + "<body>"
+                    + "<div class='container'>"
+                    + "<header>"
+                    + "    <h1>" + subject + "</h1>"
+                    + "</header>"
+                    + "<section>"
+                    + "    <p>Xin chào,</p>"
+                    + "    <p>Mã OTP của bạn là: <h2>" + otp + "</h2></p>"
+                    + "    <p>Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi.</p>"
+                    + "</section>"
+                    + "<footer>"
+                    + "    <p>Bạn nhận được email này vì bạn đã đăng ký sử dụng dịch vụ của chúng tôi.</p>"
+                    + "</footer>"
+                    + "</div>"
+                    + "</body>"
+                    + "</html>";
+
+            helper.setText(content, true);
+            javaMailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
 
