0f0067a1a7259ee26f8cfe822408da1e42b7b245
ThanhTpJava <tp.thanhjava@gmail.com>
2023-11-30 16:17:27 +0700

    update blog

--- src/main/java/com/poly/dao/ProductDAO.java
+++ src/main/java/com/poly/dao/ProductDAO.java
@@ -28,4 +28,8 @@ public interface ProductDAO extends JpaRepository<Product, Integer>{
     @Query("SELECT p FROM Product p WHERE p.name LIKE %:name%")
     List<Product> findProductsByName(@Param("name") String name);
 
+    @Query(value = "SELECT TOP 4 * FROM Products ORDER BY Date_import DESC", nativeQuery = true)
+    List<Product> findLatestProducts();
+
+
 }
