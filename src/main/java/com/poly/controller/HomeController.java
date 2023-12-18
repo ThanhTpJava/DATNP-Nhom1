5864043447ae5a587cc2fddc26b4992d6a789fa6
My PC <quocntb9399@gmail.com>
2023-12-18 07:27:20 +0700

    .

--- src/main/java/com/poly/controller/HomeController.java
+++ src/main/java/com/poly/controller/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping({"/", "/home", "/home/index"})
+	@RequestMapping({"/", "user/home", "/home/index"})
 	public String index() {
-		return "redirect:/user/home";
+		return "redirect:/";
 	}
 	
 	@RequestMapping("/admin/home/index")
